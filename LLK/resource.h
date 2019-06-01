85bd148151042ebabe88122bc4eb01484389a465
russell <906061193@qq.com>
2019-06-01 12:48:29 +0800

    添加项目文件。

---
+++ LLK/resource.h
@@ -0,0 +1,27 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 LLK.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_LLK_DIALOG                  102
+#define IDR_MAINFRAME                   128
+#define IDC_BUTTON_BASIC                1000
+#define IDC_BUTTON_RELAX                1001
+#define IDC_BUTTON_GUANQIA              1005
+#define IDC_BUTTON_RANK                 1006
+#define IDC_BUTTON_HELP                 1007
+#define IDC_BUTTON_CONFIG               1008
+#define IDB_MAIN_BG                     65535
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        137
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
